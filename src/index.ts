a51525809cad6c5f0169d5da0b72dbf261f2458f
dylangrandmont <dylan.grandmont@gmail.com>
2021-06-28 12:44:33 -0600

    mob next [ci-skip] [ci skip] [skip ci]

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ export function foo() {}
 
 export class Cell {
     isAlive: boolean = false;
-    constructor (state: boolean) {}
-    next() {
-        
+
+    constructor (state: {alive: boolean, neighbor: number}) {
+    }
+
+    next(neighbor: number) {
+        if (neighbor === 3) {
+            isAlive = true;
+        }
     }
 }
--- test/test.ts
+++ test/test.ts
@@ -4,14 +4,14 @@ import { Cell } from '../src'
 describe("something", () => {
   it('one living cell dies', () => {
     const alive = true
-    const cell = new Cell(alive)
+    const cell = new Cell({ alive:alive, neighbor: 0})
     cell.next()
     expect(cell.isAlive).equal(false)
   })
 
   it('makes a cell alive if it has three living neighbours', () => {
     const dead = false
-    const cell = new Cell(dead)
+    const cell = new Cell({ alive: dead, neighbor: 3})
     cell.next()
     expect(cell.isAlive).is.true
   })
